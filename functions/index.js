825bf78c9439345773f0d71692b981e0794fb683
ScriptedPranav <sinhafighterpranav@gmail.com>
2023-03-17 13:26:52 +0530

    v0.0.1

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const admin = require("firebase-admin");
 const { Timestamp } = require("firebase-admin/firestore");
 admin.initializeApp();
 const db = admin.firestore();
+const messaging = admin.messaging();
 
 const express = require("express");
 const cors = require("cors");
@@ -10,6 +11,31 @@ const app = express();
 
 app.use(cors({ origin: true }));
 app.use(express.json());
+
+//Create User
+app.post("/api/user/signup/:userId", async(req,res) => {
+  try {
+    await db.collection('users').doc(req.params.userId).set({
+      ...req.body,
+      timestamp: Timestamp.now()
+    })
+    res.status(200).json("User created successfully")
+  }catch(err) {
+    res.status(500).json(err)
+  }
+})
+
+//Update User
+app.put('/api/user/update/:userId', async(req,res) => {
+  try {
+    await db.collection('users').doc(req.params.userId).set({
+      ...req.body
+    },{merge:true})
+    res.status(200).json("User Updated")
+  }catch(err) {
+    res.status(500).json(err)
+  }
+})
 
 //Book Barber Appointment
 app.post("/api/book/:userId", async (req, res) => {
@@ -26,98 +52,69 @@ app.post("/api/book/:userId", async (req, res) => {
       const user = userData.data();
       const { name, hostel } = user;
 
-      const { hair, beard, massage } = req.body;
-      const serviceTime = hair * 20 + beard * 10 + massage * 10;
-
       const barberData = {
         name,
         hostel,
-        ...req.body,
-        serviceTime,
+        fcmToken: req.body.fcmToken,
         timestamp: Timestamp.now(),
       };
 
       db.collection("barber")
         .orderBy("timestamp")
         .get()
-        .then((snapshot) => {
-          let arrayR = snapshot?.docs?.map((doc) => {
-            return { barberId: doc.id, ...doc.data() };
-          });
-
-          if (arrayR.length === 0) {
-            db.collection("barber")
-              .doc(req.params.userId)
-              .set({
-                ...barberData,
-                tokenNo: arrayR.length + 1,
-                ewt: 0,
-              });
-          } else {
-            const previousPerson = arrayR[arrayR.length - 1];
-            const { serviceTime, ewt } = previousPerson;
-            const newEstimatedTime = serviceTime + ewt;
-
-            db.collection("barber")
-              .doc(req.params.userId)
-              .set({
-                ...barberData,
-                tokenNo: arrayR.length + 1,
-                ewt: newEstimatedTime,
-              });
-          }
+        .then(async(snapshot) => {
+          let count = snapshot?.docs?.length;
+          const set1 =  db.collection("barber")
+            .doc(req.params.userId)
+            .set({
+              ...barberData,
+              queue_no: count + 1,
+              token_no: count + 1,
+            });
+          const set2 = db.collection("globalVariable")
+            .doc("barber")
+            .set({
+              queue_length: count + 1,
+            });
+            await Promise.all([set1,set2])
+            res.status(200).json("booking created");
         });
-      res.status(200).json("booking created");
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
     }
   } else {
-    res.status(203).json("booking exists")
+    res.status(203).json("booking exists");
   }
 });
 
-// exports.appointmentAdded = functions.firestore
-//   .document("/barber/{documentId}")
-//   .onCreate((current, context) => {
-//     db.collection("barber")
-//       .orderBy("timestamp")
-//       .get()
-//       .then((snapshot) => {
-//         let arrayR = snapshot.docs.map((doc) => {
-//           return { barberId: doc.id, ...doc.data() };
-//         });
-//         // console.log(arrayR[arrayR.length-2]);
-//         if (arrayR.length === 1) {
-//           db.doc(`/barber/${current.id}`).set(
-//             {
-//               ...current.data(),
-//               tokenNo: arrayR.length,
-//               ewt: 0,
-//             },
-//             { merge: true }
-//           );
-//         } else {
-//           const previousPerson = arrayR[arrayR.length - 2];
-//           const { serviceTime, ewt } = previousPerson;
-//           const newEstimatedTime = serviceTime + ewt;
-//           // console.log(newEstimatedTime)
-//           db.doc(`/barber/${current.id}`).set(
-//             {
-//               ...current.data(),
-//               tokenNo: arrayR.length,
-//               ewt: newEstimatedTime,
-//             },
-//             { merge: true }
-//           );
-//         }
-//       })
-//       .catch((err) => console.log(err));
+//Create Order
+app.post("/api/order/:userId", async (req, res) => {
+  try {
+    const { name, ...rest } = req.body;
+    const userOrder = {
+      ...rest,
+      is_delivered: false,
+      timestamp: Timestamp.now(),
+    };
 
-//     return Promise.resolve();
-//   });
+    const todayOrder = {
+      ...req.body,
+      is_delivered: false,
+      timestamp: Timestamp.now(),
+    };
+    const docRef = await db
+      .collection(`users/${req.params.userId}/orders`)
+      .add(userOrder);
+    await db.collection("todayOrders").doc(docRef.id).set(todayOrder);
+    res.status(200).json("Order created");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-//Update ewt
+//Update queue_no
 exports.appointmentDeleted = functions.firestore
   .document("/barber/{documentId}")
   .onDelete((current, context) => {
@@ -125,18 +122,20 @@ exports.appointmentDeleted = functions.firestore
     db.collection("barber")
       .orderBy("timestamp")
       .get()
-      .then((snapshot) => {
+      .then(async(snapshot) => {
         let arrayR = snapshot.docs.map((doc) => {
           return { barberId: doc.id, ...doc.data() };
         });
+        await db.collection("globalVariable").doc("barber").set({
+          queue_length: arrayR.length,
+        });
         // console.log(arrayR.length);
         for (let appointment of arrayR) {
-          if (appointment.ewt > current.data().ewt) {
+          if (appointment.queue_no > current.data().queue_no) {
             // console.log(appointment.name)
             db.doc(`/barber/${appointment.barberId}`).set(
               {
-                tokenNo: appointment.tokenNo - 1,
-                ewt: appointment.ewt - current.data().serviceTime,
+                queue_no: appointment.queue_no - 1,
               },
               { merge: true }
             );
@@ -146,4 +145,46 @@ exports.appointmentDeleted = functions.firestore
     return Promise.resolve();
   });
 
+//Send Push Notification
+exports.pushNotification = functions.firestore
+  .document("/barber/{documentId}")
+  .onWrite(async (change, context) => {
+    try {
+      //get all appointments
+      const appointments = await db
+        .collection("barber")
+        .orderBy("timestamp")
+        .get();
+      const count = appointments.docs.length;
+
+      if (count > 0) {
+        //generate payload and fcmTokens
+        const firstFour = appointments.docs.slice(0, 5);
+        const fcmTokens = [];
+        const payload = {
+          notification: {
+            title: "Your Barber Booking Is Here!🙌",
+            body: "You are up for your hair cutting✂️. Please go to the barber shop ASAP🏃‍♂️",
+          },
+        };
+        const options = {
+          priority: 'high',
+          contentAvailable: true
+        }
+        //push fcm tokens
+        firstFour.map((appointment) => {
+          const { fcmToken } = appointment.data();
+          fcmTokens.push(fcmToken);
+        });
+        //send multicast message
+        messaging
+          .sendToDevice(fcmTokens, payload,options)
+          .then((response) => console.log("Successfully sent message"))
+          .catch((err) => console.log(err));
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    return Promise.resolve();
+  });
 exports.app = functions.https.onRequest(app);
